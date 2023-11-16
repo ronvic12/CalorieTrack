c2353d068e3a7c8eeccaa6c9e71cbd5be61d2b9b
ronvic12 <58205103+ronvic12@users.noreply.github.com>
2023-11-16 11:37:06 -0800

    deleted messagex.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-
-
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />}/>
-
       </Routes>
     </BrowserRouter>
   );
--- src/messageex.js
+++
@@ -1,3 +0,0 @@
-
-
-// ReactDOM.render(<Message />, document.getElementById('root'));
